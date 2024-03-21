d4045f97fb24ee0e4335c62cf5cca27957412832
Oguzhan Yildirim <108305486+OguzhanYildirimm@users.noreply.github.com>
2024-03-21 13:36:37 +0300

    Update whatsapp_stickers_exporter.dart

--- lib/whatsapp_stickers_exporter.dart
+++ lib/whatsapp_stickers_exporter.dart
@@ -69,7 +69,7 @@ class WhatsappStickerImage {
   WhatsappStickerImage._internal(this.path);
 
   factory WhatsappStickerImage.fromAsset(String asset) {
-    return WhatsappStickerImage._internal('assets://$asset');
+    return WhatsappStickerImage._internal($asset);
   }
 
   factory WhatsappStickerImage.fromFile(String file) {
